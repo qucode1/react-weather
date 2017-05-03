c55b00faeba17f0dd5d216282393afc9faf31b91
Yannick <quinius_789@yahoo.de>
2017-05-03 18:56:23 +0000

    prepare server.js and package.json for deployment

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require("express");
 
 //Create app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if(req.headers["x-forwarded-proto"] === "http"){
+        next();
+    } else {
+        res.redirect("http://" + req.hostname + req.url);
+    }
+});
 
 app.use(express.static("public"));
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("Server is live");
+app.listen(PORT, process.env.IP, function(){
+    console.log("Server is live on " + PORT);
 });
