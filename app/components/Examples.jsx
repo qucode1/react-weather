a3c429292160e2ef0562bde0ceb45070080f7135
Yannick <quinius_789@yahoo.de>
2017-05-04 14:05:44 +0000

    add basic grid and Example Links

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require("react");
+var {Link} = require("react-router");
 
 var Examples = React.createClass({
     render: function(){
@@ -6,8 +7,16 @@ var Examples = React.createClass({
             <div>
                 <h3>Examples Component</h3>
                 <p>This is the examples page.</p>
+                <ol>
+                    <li>
+                         <Link to="/?location=Philadelphia">Philadelphia, PA</Link>
+                    </li>
+                    <li>
+                        <Link to="/?location=Rio">Rio, Brazil</Link>
+                    </li>
+                </ol>
             </div>
-            )
+        )
     }
 });
 
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -19,7 +19,11 @@ var Main = (props) => {
         <div>
             <h2>Main Component</h2>
             <Nav/>
-            {props.children}
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                   {props.children}                
+                </div>
+            </div>
         </div>
         )    
 };
